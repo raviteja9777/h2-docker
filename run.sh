4d8adf5f201aa29d82541c735ac50570a5c1e547
raviteja9777 <69463915+raviteja9777@users.noreply.github.com>
2020-08-12 01:12:22 +0530

    Add files via upload

--- run.sh
+++ run.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-docker run -itd --rm --name h2 -p 9092:9092 -p 9093:9093 h2-db
+docker volume create H2-DB-data
+
+docker run -itd -v H2-DB-data:/h2-data --name h2 -p 9092:9092 -p 9093:9093 h2-db
 
 docker exec -it h2  /opt/h2/createDb.sh
-
