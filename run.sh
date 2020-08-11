067ccda59ca824e218e1f6efb2acb3d18b156b47
raviteja9777 <69463915+raviteja9777@users.noreply.github.com>
2020-08-12 01:18:59 +0530

    Run the container by creating volume
    Also created the table

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-docker volume create H2-DB-data
+docker volume create H2-DB-store
 
-docker run -itd -v H2-DB-data:/h2-data --name h2 -p 9092:9092 -p 9093:9093 h2-db
+docker run -itd -v H2-DB-store:/h2-data --name h2 -p 9092:9092 -p 9093:9093 h2-db
 
 docker exec -it h2  /opt/h2/createDb.sh
